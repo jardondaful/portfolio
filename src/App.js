4d9c437e4fd4c8aa851e3be07517a1127bf33f50
Jordan Lin <jordanlin@esri.com>
2023-08-22 17:00:28 -0700

    eh

--- src/App.js
+++ src/App.js
@@ -24,7 +24,16 @@ function App() {
         <div className="project">
           <h3>TrailMatch: Hiking Made Easy</h3>
           <img src={images.trailmatchImage} alt="TrailMatch Project" style={{ width: '100%', display: 'block', margin: '0 auto' }}/>
-          <p>Short project description goes here.</p>
+          <p>TrailMatch harnesses the power of ChatGPT API to create an 
+             exhilarating hiking experience tailored just for you. I was responsible for the entire user interface of this project,
+             and my team was able to secure 9th place in Esri's 8th Annual Hackathon!
+          </p>
+        </div>
+        <div className="project">
+          <h4>Bruin Bites</h4>
+          <img src={images.BruinBitesImage} alt="BruinBites" style={{ width: '100%', display: 'block', margin: '0 auto' }}/>
+          <p>Bruin Bites is a website that allows UCLA students to more easily connect with their friends through food! Right off the bat, 
+             a new user can use the website to view what each restaurant/take-out option is serving, view their respective ratings, and even make them themselves. </p>
         </div>
         {/* Add more projects here */}
       </section>
--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,9 @@
 import trailmatchImage from './trailmatch.png'; // Replace with your image file
+import BruinBitesImage from './BruinBites.png'
 
 const images = {
   trailmatchImage,
+  BruinBitesImage
 };
 
 export default images;
