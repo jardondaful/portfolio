617e075a2e7dc5510a99726d460bfd87bc4543d2
JordanLin03 <jordanlin@esri.com>
2023-08-31 14:52:47 -0700

    projects are now side by side like 2 per row

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import './App.css';
 import images from './images';
 
 function App() {
-  // Declare state for the slideshow index
   const [trailMatchIndex, setTrailMatchIndex] = useState(0);
-  // Array of TrailMatch images
   const trailMatchImages = [images.trailmatchImage, images.trailMatchCardImage, images.trailMatchMapImage];
 
-  // Function to navigate through slideshow images
   const handleTrailMatchNavigation = (offset) => {
     const newIndex = trailMatchIndex + offset;
-    // Ensure index is within bounds of the images array
     setTrailMatchIndex((newIndex + trailMatchImages.length) % trailMatchImages.length);
-
   };
   
   return (
     <div className="App">
-      {/* Header */}
       <header className="App-header section-fade">
         <h1>Jordan Lin</h1>
         <p>Frontend Web Application Developer</p>
       </header>
 
-      {/* About Section */}
       <section className="about-section section-fade">
         <h2>About Me</h2>
         <p>
@@ -32,40 +25,43 @@ function App() {
         </p>
       </section>
 
-      {/* Projects Section */}
       <section className="projects-section section-fade">
         <h2>Projects</h2>
-        <div className="project">
-          <h3>TrailMatch: Hiking Made Easy</h3>
-          {/* Slideshow Controls */}
-          <div className="slideshow-container">
-            <button className="arrow-button left-arrow" onClick={() => handleTrailMatchNavigation(-1)}>←</button>
-            <img src={trailMatchImages[trailMatchIndex]} alt="TrailMatch Project" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }} />
-            <button className="arrow-button right-arrow" onClick={() => handleTrailMatchNavigation(1)}>→</button>
+        <div className="project-group">
+          <div className="project">
+            <h3>TrailMatch: Hiking Made Easy</h3>
+            <div className="slideshow-container">
+              {/* Slideshow Controls */}
+              <button className="arrow-button left-arrow" onClick={() => handleTrailMatchNavigation(-1)}>←</button>
+              <img src={trailMatchImages[trailMatchIndex]} alt="TrailMatch Project" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }} />
+              <button className="arrow-button right-arrow" onClick={() => handleTrailMatchNavigation(1)}>→</button>
+            </div>
+            <p>TrailMatch harnesses the power of ChatGPT API to create an exhilarating hiking experience tailored just for you. I was responsible for the entire user interface of this project, and my team was able to secure 9th place in Esri's 8th Annual Hackathon!</p>
           </div>
-          <p>TrailMatch harnesses the power of ChatGPT API to create an exhilarating hiking experience tailored just for you. I was responsible for the entire user interface of this project, and my team was able to secure 9th place in Esri's 8th Annual Hackathon!</p>
+
+          <div className="project">
+            <h3>Bruin Bites</h3>
+            <img src={images.BruinBitesImage} alt="BruinBites" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
+            <p>Bruin Bites is a website that allows UCLA students to more easily connect with their friends through food! Right off the bat, 
+              a new user can use the website to view what each restaurant/take-out option is serving, view their respective ratings, and even make them themselves.</p>
+          </div>
         </div>
-        <div className="project">
-          <h3>Bruin Bites</h3>
-          <img src={images.BruinBitesImage} alt="BruinBites" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
-          <p>Bruin Bites is a website that allows UCLA students to more easily connect with their friends through food! Right off the bat, 
-             a new user can use the website to view what each restaurant/take-out option is serving, view their respective ratings, and even make them themselves.</p>
-        </div>
-        <div className="project">
-          <h3>California Cities Weather Dashboard</h3>
-          <img src={images.DashboardImage} alt="DasbhoardImage" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
-          <p>
-             This was made with ArcGIS Pro and ArcGIS Dashboards, showcasing California's highest temperatures during the month of August. The interactive map highlights statewide temperature patterns and identifies major cities' hottest and coldest points, fusing application design and data analytics with the power of GIS. 
-          </p>
-        </div>
-        <div className="project">
-          <h3>LA Cell Towers Explorer</h3>
-          <img src={images.CellTowersImage} alt="CellImage" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
-          <p>test</p>
+
+        <div className="project-group">
+          <div className="project">
+            <h3>California Cities Weather Dashboard</h3>
+            <img src={images.DashboardImage} alt="DashboardImage" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
+            <p>This was made with ArcGIS Pro and ArcGIS Dashboards, showcasing California's highest temperatures during the month of August. The interactive map highlights statewide temperature patterns and identifies major cities' hottest and coldest points, fusing application design and data analytics with the power of GIS.</p>
+          </div>
+
+          <div className="project">
+            <h3>LA Cell Towers Explorer</h3>
+            <img src={images.CellTowersImage} alt="CellImage" style={{ width: '100%', display: 'block', margin: '0 auto', borderRadius: '10px' }}/>
+            <p>This was made with ArcGIS Pro and ArcGIS Web AppBuilder, showcasing all the cell towers within an inputted radius of an inputted address that's within LA.</p>
+          </div>
         </div>
       </section>
 
-      {/* Contact Section */}
       <section className="contact-section section-fade">
         <h2>Contact Me</h2>
         <p>If you're interested in collaborating or have any questions, feel free to get in touch!</p>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // You can create a separate CSS file for the Navbar styles
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/contact">Contact</Link>
-        </li>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
